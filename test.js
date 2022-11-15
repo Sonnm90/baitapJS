d540c1d1d40aad1a12417725b2a4f15702579c11
sonnm90 <kenkon42@gmail.com>
2022-11-15 20:35:54 +0700

    up

---
+++ test.js
@@ -0,0 +1,24 @@
+// for (let i = 0, j = 10; i <= 10; i++, j--) {
+//     let sum = i + j;
+//     document.write(i + " + " + j + " = " + sum);
+
+// }
+
+// let text = 'Hello';
+// while (text != 'q') {
+//     document.write(text + '<br />');
+//     text = prompt('Enter a text to print. Enter "q" to quit.')
+// }
+
+// let result = "";
+// for (let i = 1; i <= 9; i++) {
+//     result += "<tr>";
+//     for (let j = 2; j <= 9; j++) {
+//         result += "<td>" + j + " x " + i + "=" + j * i + "</td>";
+//     }
+//     result += "</tr>";
+// }
+// document.getElementById("tableDemo").innerHTML = result;
+
+
+
